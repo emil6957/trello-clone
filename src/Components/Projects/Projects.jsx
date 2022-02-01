eac8f1a4ec0b8cfafa987fe6a725cb2964d18d68
Emil <emiliano.milewski@gmail.com>
2022-02-01 01:11:34 +0000

    refactored projects css

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -7,7 +7,7 @@ export default function Projects() {
     return (
         <div className="projects">
             <div className="projects__section">
-                <h3 className="projects__section__header">Recently Viewed</h3>
+                <h3 className="projects__header">Recently Viewed</h3>
                 <div className="project__items">
                     <div className="temp-div" />
                     <div className="temp-div" />
@@ -16,17 +16,17 @@ export default function Projects() {
                 </div>
             </div>
             <div className="projects__section">
-                <h3 className="projects__section__header">Favourites</h3>
+                <h3 className="projects__header">Favourites</h3>
                 <div className="project__items">
                     <div className="temp-div" />
                 </div>
             </div>
             <div className="projects__section">
-                <h3 className="projects__section__header">All Projects</h3>
+                <h3 className="projects__header">All Projects</h3>
                 <div className="project__items">
                     <div className="temp-div" />
-                    <div className="add-new-project">
-                        <img className="plus" src={plus} alt="plus" />
+                    <div className="projects__new-project">
+                        <img className="projects__plus" src={plus} alt="plus" />
                         <NewProject />
                     </div>
                 </div>
