47f547adbd9c3f085924bb12cc62f02c704abcdd
Emil <emiliano.milewski@gmail.com>
2022-02-01 01:04:44 +0000

    refactored getstarted css

--- src/Components/GetStarted/GetStarted.jsx
+++ src/Components/GetStarted/GetStarted.jsx
@@ -5,11 +5,11 @@ import google from "../../Images/google-icon.svg";
 export default function GetStarted({ signIn }) {
     return (
         <div className="get-started">
-            <div className="get-started__container">
+            <div className="get-started__inner">
                 <h1 className="get-started__title">A new way to organise your work</h1>
                 <p className="get-started__paragraph">
                     Have a new way to organise and manage all your projects
-                    with our simple and easy to use yet powerful site. You’re
+                    with our simple and easy to use yet powerful site. You&apos;re
                     only one click away from getting to work.
                 </p>
                 <button type="button" className="get-started__btn" onClick={signIn}>
