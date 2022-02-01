54054d4dfbc4805df18e27446d4f2f8123133da5
Emil <emiliano.milewski@gmail.com>
2022-02-01 01:09:39 +0000

    reafactored home css

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -6,8 +6,8 @@ export default function Home() {
     return (
         <div className="home">
             <div className="home__caught-up">
-                <h2 className="home__caught-up__header">You&apos;re already all caught up</h2>
-                <p className="home__caught-up-paragraph">
+                <h2 className="home__header">You&apos;re already all caught up</h2>
+                <p className="home__paragraph">
                 Your cards close to their
                 due date and any activity
                 on your projects will show up here
