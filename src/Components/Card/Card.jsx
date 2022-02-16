5a46f3509eee4b44783f734da0983dd1642730fd
Emil <emiliano.milewski@gmail.com>
2022-02-16 13:46:28 +0000

    Added editing functionality for cards

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -10,9 +10,14 @@ import {
     limit,
     QuerySnapshot,
     getDocs,
+    updateDoc,
+    doc,
 } from "firebase/firestore";
 import "./Card.css";
 import bin from "../../Images/bin.svg";
+import edit from "../../Images/edit.svg";
+import cross from "../../Images/x.svg";
+import tick from "../../Images/tick.svg";
 
 export default function Card({
     id,
@@ -23,6 +28,8 @@ export default function Card({
 }) {
     const [showOptions, setShowOptions] = useState(false);
     const [cardDocId, setCardDocId] = useState();
+    const [editing, setEditing] = useState(false);
+    const [newName, setNewName] = useState(name);
 
     const db = getFirestore();
 
@@ -30,20 +37,48 @@ export default function Card({
         const cardsRef = collection(db, `users/BUhOFZWdEbuKVU4FIRMg/projects/${projectDocId}/lists/${listDocId}/cards`);
         const cardQuery = query(cardsRef, where("id", "==", id), limit(1));
 
-        const querySnapshot = getDocs(cardQuery)
+        getDocs(cardQuery)
             .then((snapshot) => {
                 setCardDocId(snapshot.docs[0].id);
             });
     }, []);
 
+    function editCard() {
+        console.log("editing");
+        updateDoc(doc(db, `users/BUhOFZWdEbuKVU4FIRMg/projects/${projectDocId}/lists/${listDocId}/cards/${cardDocId}`), {
+            name: newName,
+        });
+    }
+
+    function toggleEditing() {
+        setEditing((prevBool) => !prevBool);
+    }
+
+    function handleChange(e) {
+        const { value } = e.target;
+        setNewName(value);
+    }
+
     return (
-        <div onMouseEnter={() => setShowOptions(true)} onMouseLeave={() => setShowOptions(false)} className="card">
-            <h4 className="card__name">{name}</h4>
-            {showOptions && (
-                <div className="card__options">
-                    <img onClick={() => deleteCard(listDocId, cardDocId)} className="card__delete" src={bin} alt="bin" />
+        editing ? (
+            <div className="card__editing">
+                <input onChange={(e) => handleChange(e)} className="card__edit-input" value={newName} />
+                <div className="card__edit-options">
+                    <img onClick={() => { editCard(); toggleEditing(); }} className="card__tick" src={tick} alt="tick" />
+                    <img onClick={() => { toggleEditing(); setNewName(name); }} className="card__cross" src={cross} alt="cross" />
                 </div>
-            )}
-        </div>
+            </div>
+        )
+            : (
+                <div onMouseEnter={() => setShowOptions(true)} onMouseLeave={() => setShowOptions(false)} className="card">
+                    <h4 className="card__name">{name}</h4>
+                    {showOptions && (
+                        <div className="card__options">
+                            <img onClick={() => deleteCard(listDocId, cardDocId)} className="card__delete" src={bin} alt="bin" />
+                            <img onClick={() => toggleEditing()} className="card__edit" src={edit} alt="edit" />
+                        </div>
+                    )}
+                </div>
+            )
     );
 }
