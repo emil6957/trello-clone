b221f8dab9542ebd9fab42a38e1902cbe7550c46
Emil <emiliano.milewski@gmail.com>
2022-07-07 15:58:59 +0100

    added functionality to the recent button in the header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
 import HeaderProfile from "../HeaderProfile/HeaderProfile";
 import chervron from "../../Images/chevron-left.svg";
+import HeaderRecent from "../HeaderRecent/HeaderRecent";
 
 export default function Header({ currentUser, signIn, signOutUser }) {
+    const [showRecents, setShowRecents] = useState(false);
+
+    function handleRecents() {
+        setShowRecents((prevState) => !prevState);
+    }
+
     return (
         <header className="header">
             <h2 className="header__title"><Link to="/" className="header__home-btn">Trollo</Link></h2>
@@ -12,10 +19,10 @@ export default function Header({ currentUser, signIn, signOutUser }) {
                 currentUser
                     && (
                         <div className="header__btns">
-                            <div className="header__btn header__recent">
+                            <button type="button" className="header__btn header__recent" onClick={handleRecents}>
                                 <p className="header__recent-text">Recent</p>
                                 <img className="header__recent-img" src={chervron} alt="chevron" />
-                            </div>
+                            </button>
                             <Link to="/projects" className="header__btn">Projects</Link>
                             <p className="header__btn header__create-text">Create</p>
                         </div>
@@ -25,6 +32,11 @@ export default function Header({ currentUser, signIn, signOutUser }) {
                 currentUser
                     ? <HeaderProfile currentUser={currentUser} signOutUser={signOutUser} />
                     : <button type="button" className="header__login" onClick={currentUser ? signOutUser : signIn}>Log In</button>
+            }
+            {
+                showRecents && (
+                    <HeaderRecent handleRecents={() => handleRecents()} />
+                )
             }
         </header>
     );
--- src/Components/HeaderRecent/HeaderRecent.jsx
+++ src/Components/HeaderRecent/HeaderRecent.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./HeaderRecent.css";
+import { Link } from "react-router-dom";
 import {
     collection,
     getDocs,
@@ -7,8 +8,9 @@ import {
     orderBy,
     query,
 } from "firebase/firestore";
+import cross from "../../Images/x.svg";
 
-export default function HeaderRecent() {
+export default function HeaderRecent({ handleRecents }) {
     const [recentProjects, setRecentProjects] = useState([]);
     const db = getFirestore();
 
@@ -20,15 +22,32 @@ export default function HeaderRecent() {
                 const data = snapshot.docs.map((document) => ({
                     ...document.data(),
                 }));
-                setRecentProjects([data]);
+                setRecentProjects(data);
             });
     }, []);
 
-    useEffect(() => {
-        console.log(recentProjects);
-    }, [recentProjects]);
+    const recentProjectElements = recentProjects.map((project) => (
+        <Link to={`projects/${project.id}`} className="header-recent__project" key={project.id}>
+            <div
+                style={{ background: project.background, backgroundRepeat: "no-repeat" }}
+                className="header-recent__project-img"
+            />
+            <p className="header-recent__project-name">{project.name}</p>
+        </Link>
+    ));
 
     return (
-        <div />
+        <div className="header-recent">
+            <div className="header-recent__header">
+                <p className="header-recent__title">Recent Boards</p>
+                <button type="button" className="header-recent__close-btn" onClick={handleRecents}>
+                    <img src={cross} alt="cross" />
+                </button>
+            </div>
+            <hr />
+            <div className="header-recent__projects">
+                { recentProjects.length ? recentProjectElements : <p>You have no projects</p> }
+            </div>
+        </div>
     );
 }
