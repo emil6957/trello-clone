4d42a398c49fb99854ecba8a8baf1928ee51f580
Emil <emiliano.milewski@gmail.com>
2024-01-10 16:33:19 +0000

    profile dropdown now closes after clicking outside of it

--- src/Components/HeaderProfile/HeaderProfile.jsx
+++ src/Components/HeaderProfile/HeaderProfile.jsx
@@ -1,25 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import ProfileDropdown from "../ProfileDropdown/ProfileDropdown";
 import "./HeaderProfile.css";
 
 export default function HeaderProfile({ currentUser, signOutUser }) {
     const [showDropdown, setShowDropdown] = useState(false);
 
+    const profileRef = useRef();
+
     function toggleDropdown() {
         setShowDropdown((prevBool) => !prevBool);
     }
 
+    useEffect(() => {
+        const handler = (e) => {
+            if (!profileRef.current.contains(e.target)) {
+                setShowDropdown(false);
+            }
+        };
+
+        document.addEventListener("mousedown", handler);
+
+        return () => {
+            document.removeEventListener("mousedown", handler);
+        };
+    });
+
     return (
-        <div className="profile">
+        <div className="profile" ref={profileRef}>
             <button className="profile__button" type="button" onClick={toggleDropdown}>
                 <img className="profile__picture" src={currentUser.photoURL} alt="user profile" />
             </button>
-            {showDropdown && (
+            <div className={`profile__dropdown-container ${showDropdown ? "active" : "inactive"}`}>
                 <ProfileDropdown
                     currentUser={currentUser}
                     signOutUser={signOutUser}
                 />
-            )}
+            </div>
         </div>
     );
 }
