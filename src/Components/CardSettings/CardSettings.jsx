9b4f4cfcd84b95b6af942599e53f608533e71bf1
Emil <emiliano.milewski@gmail.com>
2024-01-13 21:51:36 +0000

    added CardSettings component

---
+++ src/Components/CardSettings/CardSettings.jsx
@@ -0,0 +1,12 @@
+import "./CardSettings.css";
+
+export default function CardSettings() {
+    return (
+        <div className="card-settings">
+            <div className="card-settings__title">
+                <h2 className="card-settings__card-name"></h2>
+                <p className="card-settings__project-name"></p>
+            </div>
+        </div>
+    );
+};
