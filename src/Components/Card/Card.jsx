1c0d67da959340cfc89ea623d751c9358437e448
Emil <emiliano.milewski@gmail.com>
2024-01-22 23:37:14 +0000

    .

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable jsx-a11y/mouse-events-have-key-events */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable prefer-template */
 
 import React, { useEffect, useState } from "react";
 import {
